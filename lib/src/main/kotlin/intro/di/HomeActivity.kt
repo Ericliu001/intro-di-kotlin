81a37571289cc2cf926563d77f403f31ec6880fd
Eric Liu <eric.liu@uber.com>
2022-06-23 14:41:14 -0700

    ready for exercise

--- lib/src/main/kotlin/intro/di/HomeActivity.kt
+++ lib/src/main/kotlin/intro/di/HomeActivity.kt
@@ -1,26 +1,18 @@
 package intro.di
 
 class HomeActivity {
-    private var profile: Profile? = null
-    lateinit var rootScope: RootScope
-
     fun onCreate() {
-        rootScope = RootScopeImpl()
     }
 
     /**
      * Login the user.
-     *//**/
+     */
     fun login() {
-        profile = rootScope.authService().login("eric.liu@uber.com", "xxxx")
     }
 
     /**
      * Request a ride.
      */
     fun requestRide() {
-        profile?.let {
-            rootScope.loggedInScope(it).rideRequestService().requestRide()
-        }
     }
 }
